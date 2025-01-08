9f28d6e4b929462836d52fb6aa43e7353a77213b
nabilulilalbab <nabilulilalbab78@gmail.com>
2025-01-08 23:31:45 +0700

    Initial commit production ready

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,13 +1,17 @@
 
 from django.contrib import admin
-from django.urls import path,include
 from .views import index,about
+from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve
 urlpatterns = [
+re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
     path("about/",about,name="about"),
     path("olshopthrift/",include("olshopthrift.urls",namespace="olshopthrift")),
     path("",index , name="index"),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
